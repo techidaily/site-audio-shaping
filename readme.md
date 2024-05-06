c4dea85bf64b497fcbae5436d3a056a6aa3e2e32
lauer3912 <lauer3912@gmail.com>
2024-05-06 09:22:07 +0800

    init

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
-# TechiDaily
+# Audio Shaping Site
 
-Welcome to [https://android-transfer.techidaily.com](https://android-transfer.techidaily.com)
+Welcome to [https://audio-shaping.techidaily.com](https://audio-shaping.techidaily.com)
 
 ## Publish
 
--- submit.bing.js
+++ submit.bing.js
@@ -8,8 +8,8 @@ const fs = require('fs');
 const path = require('path');
 const https = require('https');
 
-const indexNowKey = 'ec9c9516ea82433086d979e16e1960e0'; // 替换为你的API密钥
-const siteHostName = 'android-transfer.techidaily.com'; // 替换为你的网站URL
+const indexNowKey = '548ad626b7884e2485dbcbf5013d572c'; // 替换为你的API密钥
+const siteHostName = 'audio-shaping.techidaily.com'; // 替换为你的网站URL
 const groupItemCount = 100; // 每组最多个URL
 
 
